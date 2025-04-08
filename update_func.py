1378e07fb4f87008944090185678e955b1bd4fe4
pluto <analog@phaser>
2025-04-08 15:23:54 -0400

    Added initial code to update_func.py

---
+++ update_func.py
@@ -0,0 +1,50 @@
+# Author: Garrett Brindle
+# Created: 04/08/25
+# Last Edit: 04/08/25
+
+# Using a portion of the update function from the standard test program
+# Call update() when you want to take image
+# Call load() when you are done imaging and what to create .mat file
+# CURRENTLY DRAFT - do not worry about uninstantiated variables
+# Meant for use inside larger program
+
+import numpy as np
+from scipy.io import savemat
+
+# Create global array to share with load and update
+# 80 x 300 array with complex int or float type
+
+
+def load():
+    """
+    Uses scipy library
+    Saves 2D array to .mat file
+    """
+    mdic = {"": temp_array, "label": "Data_04_08_25"}
+
+def update():
+    """
+    Tells SDR to capture data
+    Removes non-linear portion of received signal
+    Preforms preliminary data storage in 2D array
+	"""
+    my_phaser._gpios.gpio_burst = 0
+    my_phaser._gpios.gpio_burst = 1
+    my_phaser._gpios.gpio_burst = 0
+    data = my_sdr.rx()
+    chan1 = data[0]
+    chan2 = data[1]
+    sum_data = chan1 + chan2
+
+    # select just the linear portion of the last chirp
+    rx_bursts = np.zeros((num_chirps, good_ramp_samples), dtype=complex)
+    for burst in range(num_chirps):
+        start_index = start_offset_samples + burst * num_samples_frame
+        stop_index = start_index + good_ramp_samples
+        rx_bursts[burst] = sum_data[start_index:stop_index]
+        burst_data = np.ones(fft_size, dtype=complex) * 1e-10
+        # win_funct = np.blackman(len(rx_bursts[burst]))
+        win_funct = np.ones(len(rx_bursts[burst]))
+        burst_data[start_offset_samples:(start_offset_samples + good_ramp_samples)] = rx_bursts[burst] * win_funct
+    
+    # Save the linear portion to array for later save
